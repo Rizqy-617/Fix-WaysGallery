92da808f601cc78e5351898f0886c990e8e65da8
Rizqy-617 <112744959+Rizqy-617@users.noreply.github.com>
2023-03-01 05:22:18 +0700

    Update post.go

--- server/models/post.go
+++ server/models/post.go
@@ -7,7 +7,7 @@ type Post struct {
 	CreatedAt   time.Time   `json:"-" gorm:"type: TIMESTAMP DEFAULT CURRENT_TIMESTAMP"`
 	UpdatedAt   time.Time   `json:"-" gorm:"type: TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
 	Title       string      `json:"title" gorm:"type:varchar(255)"`
-	Description string      `json:"description" gorm:"type:varchar(255)"`
+	Description string      `json:"description" gorm:"type:text"`
 	CreatedBy   int         `json:"created_by"`
 	User        User        `json:"user" gorm:"foreignKey:CreatedBy"`
 	PostImage   []PostImage `json:"post_image" gorm:"foreignKey:PostID"`
