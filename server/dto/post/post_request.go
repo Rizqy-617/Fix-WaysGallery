24f7211ed009707eb9ee1e4c769be85609dd02e2
Rizqy-617 <112744959+Rizqy-617@users.noreply.github.com>
2023-03-01 05:21:39 +0700

    Update post_request.go

--- server/dto/post/post_request.go
+++ server/dto/post/post_request.go
@@ -2,5 +2,5 @@ package postdto
 
 type CreatePostRequest struct {
 	Title       string `json:"title" gorm:"type: varchar(255)" validate:"required"`
-	Description string `json:"description" gorm:"type: varchar(255)" validate:"required"`
+	Description string `json:"description" gorm:"type: text" validate:"required"`
 }
